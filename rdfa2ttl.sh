2a24d9374945abee3364ff7d947cf5fcd6fb158e
Evgeny Cherkashin <eugeneai@irnok.net>
2017-03-24 15:53:36 +0800

    Added debugging tools.

--- rdfa2ttl.sh
+++ rdfa2ttl.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python2 pyrdfa3/scripts/localRDFa.py -s -b https://irnok.net/pages/standard-09.03.01-bachelor-2016.html ./standard-09.03.01-bachelor-2016.html > standard-09.03.01-bachelor-2016.ttl
-python2 pyrdfa3/scripts/localRDFa.py -s -b http://irnok.net/pages/Work-Program-AIIS-Academic-Bachelor.html ./Work-Program-AIIS-Academic-Bachelor.html > Work-Program-AIIS-Academic-Bachelor.ttl
+python2 pyrdfa3/scripts/localRDFa.py -s -b https://irnok.net/app/legalunit/notarial/letter-of-attornity.html ./index.html > letter-of-attornity.ttl
+# python2 pyrdfa3/scripts/localRDFa.py -s -b http://irnok.net/pages/Work-Program-AIIS-Academic-Bachelor.html ./Work-Program-AIIS-Academic-Bachelor.html > Work-Program-AIIS-Academic-Bachelor.ttl
