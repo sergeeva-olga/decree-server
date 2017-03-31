5fd88822067dc0bd27f3b502209f3c0e3fd7f26a
Evgeny Cherkashin <eugeneai@irnok.net>
2017-03-31 17:04:27 +0800

    Shift content into the module.

--- src/isu/aquarium/server.py
+++ src/isu/aquarium/server.py
@@ -8,9 +8,17 @@ def hello_world(request):
     return 'Hello World'
 
 
+def static_path(dir):
+    return 'isu.aquarium:' + 'client/' + dir
+
+
 def main(config, **settings):
     config = Configurator(settings=settings)
     config.add_route('hello', '/')
+
+    for asset in """assets css dist fonts images js""".split():
+        config.add_static_view(name=asset, path=static_path(asset))
+
     config.scan()
     app = config.make_wsgi_app()
     return app
